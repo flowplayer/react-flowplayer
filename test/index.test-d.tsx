f0336365ee7081252551dc16fd757ac790cbf223
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-05-06 14:56:32 +0300

    chore: add typespec tests

---
+++ test/index.test-d.tsx
@@ -0,0 +1,63 @@
+import {expectType, expectAssignable, expectError} from "tsd"
+import Flowplayer, {useFlowplayer} from "../lib"
+import ReactDom from "react-dom";
+import React, { useEffect, useRef, useState } from "react";
+
+import { PAUSE, PLAYING } from "@flowplayer/player/core/events";
+import { Player } from "@flowplayer/player";
+
+// - useFlowplayer returns Player instance - 
+const playerRef = useRef<HTMLDivElement>(null);
+const playerApi = useFlowplayer(playerRef);
+expectType<Player | null>(playerApi)
+
+// - basic initalization - 
+const _uplayer = <Flowplayer
+    src={"demoSrc"}
+    token={"DEMO_TOKEN"}
+    ref={playerRef}
+  />
+
+// - missing src throws err - 
+expectError(<Flowplayer
+  token={"DEMO_TOKEN"}
+  ref={playerRef}
+/>)
+
+// - opts basic - 
+const _uplayerWithOpts = <Flowplayer
+  src={"demoSrc"}
+  token={"DEMO_TOKEN"}
+  ref={playerRef}
+  opts={{
+    title: "Example title",
+    description: "Example description",
+    autopause: true
+  }}
+/>
+
+// - opts with plugins - 
+const _uplayerWithPlugins =  <Flowplayer
+  src={"demoSrc"}
+  token={"DEMO_TOKEN"}
+  ref={playerRef}
+  opts={{
+    title: "Example title",
+    description: "Example description",
+    cueponts: [{
+      startTime: 1,
+      endTime: 2,
+      text: "df"
+    }]
+  }}
+/>
+
+// - opts wrong types  - 
+expectError(<Flowplayer
+  src={"demoSrc"}
+  token={"DEMO_TOKEN"}
+  ref={playerRef}
+  opts={{
+    title: 2
+  }}
+/>)
